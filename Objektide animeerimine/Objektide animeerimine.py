30708c7bf136d19a9f75e3edeedf0d4d3d04e2ba
rainer.tamm <rainer.tamm@vikk.ee>
2026-03-11 12:03:08 +0200

    Uuendatud versioon

--- Objektide animeerimine/Objektide animeerimine.py
+++ Objektide animeerimine/Objektide animeerimine.py
@@ -4,91 +4,86 @@ import random
 
 pygame.init()
 
-# --- 1. SEADED JA PILTIDE LAADIMINE ---
+# 1. SEADED JA PILTIDE LAADIMINE
 screenX = 640
 screenY = 480
 screen = pygame.display.set_mode([screenX, screenY])
 pygame.display.set_caption("Autode animatsioon ja skoor")
 clock = pygame.time.Clock()
 
-# Värv teksti jaoks
 MUST = [0, 0, 0]
 
-# Piltide laadimine
 taust = pygame.image.load("bg_rally.jpg")
 punane_auto = pygame.image.load("f1_red.png")
 sinine_auto = pygame.image.load("f1_blue.png")
 
-# --- 2. MUUTUJAD JA ALGSEADED ---
-# Punase auto asukoht (alla keskele)
+# 2. MUUTUJAD JA ALGSEADED
 punane_laius = punane_auto.get_rect().width
-punane_x = (screenX / 2) - (punane_laius / 2)  # Keskel
+sinine_laius = sinine_auto.get_rect().width
+
+tee_vasak_serv = 140
+tee_parem_serv = 500
+
+# Radade arvutamine
+tee_laius = tee_parem_serv - tee_vasak_serv
+raja_laius = tee_laius / 3
+
+rada1_x = tee_vasak_serv + (raja_laius / 2) - (sinine_laius / 2)  # Vasak
+rada2_x = tee_vasak_serv + raja_laius + (raja_laius / 2) - (sinine_laius / 2)  # Keskmine
+rada3_x = tee_vasak_serv + (raja_laius * 2) + (raja_laius / 2) - (sinine_laius / 2)  # Parem
+
+# Määrame punase auto täpselt keskmisele rajale!
+punane_x = rada2_x
 punane_y = 380
 
-# Skoori muutuja
+# Teeme nimekirja ainult vasakust ja paremast rajast siniste autode jaoks
+rajad_sinistele = [rada1_x, rada3_x]
+
 skoor = 0
-# Fondi seadistamine skoori jaoks
 font = pygame.font.SysFont("Arial", 36)
 
-# Siniste autode massiivi loomine (hoiame koordinaate ja kiirust)
-# Õppematerjali eeskujul: coords = [[x, y, kiirus], [x, y, kiirus], ...]
 sinised_autod = []
 
-# Loome alguses näiteks 3 sinist autot suvalistesse kohtadesse
-for i in range(3):
-    # Autod jäävad tee vahemikku (tee x on umbes 150 kuni 450 vahel, sõltub pildist)
-    x = random.randint(150, 450)
-    # Alustavad erinevatelt kõrgustelt (isegi ekraanist kõrgemal, et kohe kõik korraga ei tuleks)
-    y = random.randint(-400, -50)
-    # Suvaline kiirus
+#Loome 2 sinist autot (üks vasakule, teine paremale)
+for i in range(2):
+    x = rajad_sinistele[i]  # Esimene auto saab raja1_x, teine saab rada3_x
+    y = random.randint(-600, -100)
     kiirus = random.randint(2, 5)
-
-    # Lisame auto andmed listi
     sinised_autod.append([x, y, kiirus])
 
-# --- 3. PEATSÜKKEL ---
+#3. PEATSÜKKEL
 gameover = False
 while not gameover:
-    clock.tick(60)  # 60 kaadrit sekundis
+    clock.tick(60)
 
-    # Ristist sulgemise kontroll
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    # --- 4. GRAAFIKA JOONISTAMINE JA LOOGIKA ---
-
-    # 4.1. Tausta joonistamine (see kustutab eelmise kaadri)
+    #4. GRAAFIKA JOONISTAMINE JA LOOGIKA
     screen.blit(taust, (0, 0))
-
-    # 4.2. Punase auto joonistamine
     screen.blit(punane_auto, (punane_x, punane_y))
 
-    # 4.3. Siniste autode animatsioon ja loogika
     for i in range(len(sinised_autod)):
-        # Joonistame sinise auto tema [x, y] koordinaatidele
         screen.blit(sinine_auto, (sinised_autod[i][0], sinised_autod[i][1]))
-
-        # Liigutame autot allapoole vastavalt tema kiirusele
-        # sinised_autod[i][1] on y-koordinaat, sinised_autod[i][2] on kiirus
         sinised_autod[i][1] += sinised_autod[i][2]
 
         # Kui sinine auto jõuab ekraani alla
         if sinised_autod[i][1] > screenY:
-            skoor += 1  # Lisame skoori!
+            skoor += 1
 
-            # Saadame auto uuesti üles ja muudame x-asukohta
-            sinised_autod[i][1] = random.randint(-200, -50)  # y-asukoht üleval
-            sinised_autod[i][0] = random.randint(150, 450)  # Uus x-asukoht teel
-            sinised_autod[i][2] = random.randint(2, 5)  # Uus suvaline kiirus
+            # Saadame auto uuesti üles
+            sinised_autod[i][1] = random.randint(-300, -50)
 
-    # 4.4. Skoori kuvamine
-    # Teisendame arvu tekstiks: str(skoor)
+            # Hoiame auto endiselt tema isiklikul rajal (ainult äärmised rajad)
+            sinised_autod[i][0] = rajad_sinistele[i]
+            sinised_autod[i][2] = random.randint(2, 5)
+
+    # Skoori kuvamine
     skoor_tekst = font.render("Skoor: " + str(skoor), True, MUST)
-    # Joonistame teksti ekraanile (üles vasakule nurka)
     screen.blit(skoor_tekst, (10, 10))
 
-    # --- 5. EKRAANI UUENDAMINE ---
+    # 5. EKRAANI UUENDAMINE
     pygame.display.flip()
 
 pygame.quit()
